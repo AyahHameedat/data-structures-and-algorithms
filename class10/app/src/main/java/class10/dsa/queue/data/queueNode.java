0a95494c6747e28e9e5bc101b903e9e95a54e969
AyaJas <aya.hameedat@gmail.com>
2022-03-17 04:05:09 +0300

    Code Challenge10

---
+++ class10/app/src/main/java/class10/dsa/queue/data/queueNode.java
@@ -0,0 +1,31 @@
+package class10.dsa.queue.data;
+
+public class queueNode {
+
+    private final String carName;
+    private queueNode next;
+
+    public queueNode(String carName) {
+    this.carName = carName;
+    }
+
+    public void setNext(queueNode next) {
+        this.next = next;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public queueNode getNext() {
+        return next;
+    }
+
+    @Override
+    public String toString() {
+        return "queueNode{" +
+                "carName='" + carName + '\'' +
+                '}';
+    }
+}
+
---
+++ class10/app/src/main/java/class10/dsa/stack/data/stackNode.java
@@ -0,0 +1,32 @@
+package class10.dsa.stack.data;
+
+public class stackNode {
+
+    private final String carName;
+    private stackNode next;
+
+
+    public stackNode(String carName) {
+        this.carName = carName;
+    }
+
+    public void setNext(stackNode next) {
+        this.next = next;
+    }
+
+    public String getCar() {
+        return carName;
+    }
+
+    public stackNode getNext() {
+        return next;
+    }
+
+
+    @Override
+    public String toString() {
+        return "stackNode{" +
+                "car='" + carName + '\''+
+                '}';
+    }
+}
