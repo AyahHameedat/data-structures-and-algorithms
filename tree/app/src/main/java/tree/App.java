b3dcad1f9a534027ff7da7b95a0d2bdbc99ecfea
AyaJas <aya.hameedat@gmail.com>
2022-03-30 23:28:27 +0300

    Code Challenge15

--- tree/app/src/main/java/tree/App.java
+++ tree/app/src/main/java/tree/App.java
@@ -10,5 +10,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        System.out.println("AYA ");
+        System.out.println("Code Challenge 15");
+
     }
 }
