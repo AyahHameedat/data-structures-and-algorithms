e13e0385b5450d00ff7a84197ece168bb44597d7
AyahHameedat <aya.hameedat@gmail.com>
2022-06-06 19:44:52 +0300

    Code challenge 32 Done

---
+++ HashTable/src/main/java/data/Node.java
@@ -0,0 +1,41 @@
+package data;
+
+public abstract class Node<Integer extends Comparable<Integer>> implements Comparable<Node<Integer>> {
+        private final Integer data;
+        private Node<Integer> leftNode;
+        private Node<Integer> rightNode;
+
+        public Node(Integer data) {
+            this.data = data;
+        }
+
+        public Integer getData() {
+            return data;
+        }
+
+        public Node<Integer> getLeftNode() {
+            return leftNode;
+        }
+
+        public Node<Integer> getRightNode() {
+            return rightNode;
+        }
+
+        public void setLeftNode(Node<Integer> leftNode) {
+            this.leftNode = leftNode;
+        }
+
+        public void setRightNode(Node<Integer> rightNode) {
+            this.rightNode = rightNode;
+        }
+
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", leftNode=" + leftNode +
+                    ", rightNode=" + rightNode +
+                    '}';
+        }
+}
